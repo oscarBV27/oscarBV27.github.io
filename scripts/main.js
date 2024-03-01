c9d069fe17831c9c1b9751b9b2f510337edb9b1a
oscarBV27 <133826013+oscarBV27@users.noreply.github.com>
2024-03-01 13:20:20 -0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,21 @@ AOS.init({
 });
 
 // Add your javascript here
+$(document).ready(function() {
+    // Ajustar la altura del iframe al cargar y al cambiar el tamaño de la ventana
+    function adjustIframeHeight() {
+        var windowHeight = $(window).height();
+        var iframeOffset = $('#contactForm').offset().top;
+        var newIframeHeight = windowHeight - iframeOffset - 20; // Ajuste por píxeles para un mejor ajuste
+
+        $('#contactForm').css('height', newIframeHeight + 'px');
+    }
+
+    // Llamar a la función para ajustar la altura inicialmente
+    adjustIframeHeight();
+
+    // Volver a ajustar la altura del iframe cuando se cambie el tamaño de la ventana
+    $(window).resize(function() {
+        adjustIframeHeight();
+    });
+});
